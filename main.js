f70f23d278ee7f01b66dd4d24bf55da751557837
Francesco <fraancesco.bello@gmail.com>
2020-11-12 16:54:07 +0100

    chat container

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var App = new Vue({
   el : "#root",
   data : {
-    avatarImg :"file:///Users/francescobello/Desktop/avatar%20img/pngtree-user-vector-avatar-png-image_1541962.jpg",
-    contacts: []
+    avatarImg :"file:///Users/francescobello/Desktop/Bello%20Francesco/vue-boolzapp/avatar%20img/pngtree-user-vector-avatar-png-image_1541962.jpg",
+    contacts: [ "michele", "luigi",'Francesco','Massimo','luca','Piero', 'Samuele',
+    
+    ]
 },
 }
 )
