bb439e47b3fc748f1577842c7ce2cea36b353c4a
Francesco <fraancesco.bello@gmail.com>
2020-11-12 15:10:21 +0100

    style

---
+++ main.js
@@ -0,0 +1,8 @@
+var App = new Vue({
+  el : "#root",
+  data : {
+    avatarImg :"file:///Users/francescobello/Desktop/avatar%20img/pngtree-user-vector-avatar-png-image_1541962.jpg",
+    contacts: []
+},
+}
+)
