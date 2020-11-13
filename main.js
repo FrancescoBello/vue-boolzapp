cfefb1affa79dd3466bb0fff34f591c4bf73f6cd
Francesco <fraancesco.bello@gmail.com>
2020-11-13 11:56:14 +0100

    aggiunto avatar e chat

--- main.js
+++ main.js
@@ -1,10 +1,98 @@
 var App = new Vue({
   el : "#root",
   data : {
-    avatarImg :"file:///Users/francescobello/Desktop/Bello%20Francesco/vue-boolzapp/avatar%20img/pngtree-user-vector-avatar-png-image_1541962.jpg",
-    contacts: [ "michele", "luigi",'Francesco','Massimo','luca','Piero', 'Samuele',]
 
-    
+    avatarImg :"file:///Users/francescobello/Desktop/Bello%20Francesco/vue-boolzapp/img/myuser.jpg",
+    contacts: [
+      {
+        name: 'Michele',
+        avatar: '_1',
+        visible: true,
+        messages:[
+          {
+           date: '10/01/2020 15:30:55',
+           message: 'Hai portato a spasso il cane?',
+           status: 'sent'
+           },
+           {
+           date: '10/01/2020 15:50:00',
+           message: 'Ricordati di dargli da mangiare',
+           status: 'sent'
+           },
+           {
+           date: '10/01/2020 16:15:22',
+           message: 'Tutto fatto!',
+           status: 'received'
+         }],
+      },
+      {
+        name: 'Fabio',
+        avatar: '_2',
+        visible: true,
+        messages:[
+          {
+           date: '10/01/2020 15:30:55',
+           message: 'Hai portato a spasso il cane?',
+           status: 'sent'
+           },
+           {
+           date: '10/01/2020 15:50:00',
+           message: 'Ricordati di dargli da mangiare',
+           status: 'sent'
+           },
+           {
+           date: '10/01/2020 16:15:22',
+           message: 'Tutto fatto!',
+           status: 'received'
+         }],
+      },
+      {
+        name: 'Samuele',
+        avatar: '_3',
+        visible: true,
+        messages:[
+          {
+           date: '10/01/2020 15:30:55',
+           message: 'Hai portato a spasso il cane?',
+           status: 'sent'
+           },
+           {
+           date: '10/01/2020 15:50:00',
+           message: 'Ricordati di dargli da mangiare',
+           status: 'sent'
+           },
+           {
+           date: '10/01/2020 16:15:22',
+           message: 'Tutto fatto!',
+           status: 'received'
+         }],
+      },
+      {
+        name: 'Lorenzo',
+        avatar: '_4',
+        visible: true,
+        messages:[
+          {
+           date: '10/01/2020 15:30:55',
+           message: 'Hai portato a spasso il cane?',
+           status: 'sent'
+           },
+           {
+           date: '10/01/2020 15:50:00',
+           message: 'Ricordati di dargli da mangiare',
+           status: 'sent'
+           },
+           {
+           date: '10/01/2020 16:15:22',
+           message: 'Tutto fatto!',
+           status: 'received'
+         }],
+      },
+
+
+    ]
+
+
 },
 }
 )
