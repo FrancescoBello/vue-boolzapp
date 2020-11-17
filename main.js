529647ea0a178fe388f7769783d5ae175be12c5a
Francesco <fraancesco.bello@gmail.com>
2020-11-17 14:59:05 +0100

    prova ricerca utenti

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var App = new Vue({
     indiceMex:0,
     newMessage:"",
     indice:0,
+    search:"",
 
     avatarImg :"file:///Users/francescobello/Desktop/Bello%20Francesco/vue-boolzapp/img/myuser.jpg",
     contacts: [
@@ -91,6 +92,71 @@ var App = new Vue({
            status: 'received'
          }],
       },
+      {
+        name: 'Luca',
+        avatar: '_5',
+        visible: true,
+        messages:[
+          {
+           date: '10/01/2020 15:30:55',
+           message: 'Hai portato il cane?',
+           status: 'sent'
+           },
+           {
+           date: '10/01/2020 15:50:00',
+           message: 'Ricordati di dargli da mangiare',
+           status: 'sent'
+           },
+           {
+           date: '10/01/2020 16:15:22',
+           message: 'Tutto fatto!',
+           status: 'received'
+         }],
+      },
+      {
+        name: 'Pasquale',
+        avatar: '_6',
+        visible: true,
+        messages:[
+          {
+           date: '10/01/2020 15:30:55',
+           message: 'Mangiamo fuori stasera?',
+           status: 'sent'
+           },
+           {
+           date: '10/01/2020 15:50:00',
+           message: 'Ti va?',
+           status: 'sent'
+           },
+           {
+           date: '10/01/2020 16:15:22',
+           message: 'Si va bene',
+           status: 'received'
+         }],
+      },
+
+
+      {
+        name: 'Ale',
+        avatar: '_7',
+        visible: true,
+        messages:[
+          {
+           date: '10/01/2020 15:30:55',
+           message: 'Sei andato dalla nonna?',
+           status: 'sent'
+           },
+           {
+           date: '10/01/2020 15:50:00',
+           message: 'Ricordati di dargli le medicine',
+           status: 'sent'
+           },
+           {
+           date: '10/01/2020 16:15:22',
+           message: 'Tutto fatto!',
+           status: 'received'
+         }],
+      },
 
 
     ]
@@ -110,22 +176,33 @@ var App = new Vue({
                 date: '16/11/2020',
                 status: 'sent'
             };
+            let activeChat = this.contacts[this.indiceMex].messages;
 
-        this.contacts[this.indiceMex].messages.push(nuovoMex);
+        activeChat.push(nuovoMex);
         this.newMessage = "";
-        if (this.newMessage != " ") {
-           setTimeout(function () {
-             console.log("time");
+
+           setTimeout(function() {
              let receivedMessage = {
-               message : "ok",
-               date: 16/11/2020,
+               message: "ok",
+               date: '16/11/2020',
                status: 'received'
-             } 
+             };
+             activeChat.push(receivedMessage);
+
            },1000);
-
-
-        }
       },
+      searchItem(){
+      this.contacts.forEach((contact, i) => {
+        if (contact.name.includes(this.search)) {
+          contact.visible = true;
+        }else {
+          contact.visible = false;
+        }
+      });
+      console.log(this.search);
+
+
+      }
 
 
 
